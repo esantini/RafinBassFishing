8a56388bf8e052a4fa97de85dac591c34d50ae92
Esteban <esantinie@gmail.com>
2021-08-07 15:44:20 -0700

    gallery construction stamp

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Gallery() {
@@ -5,6 +6,9 @@ export default function Gallery() {
     <section className={styles.gallery} id='gallery'>
       <h2>GALLERY</h2>
       <p>Click on image to zoom-in. Enjoy!</p>
+      <div style={{ position: 'absolute', top: '100px' }}>
+        <Image src='/construction.png' width='200px' height='100px' />
+      </div>
     </section>
   );
 }
